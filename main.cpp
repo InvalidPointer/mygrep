15a42781b056c7e0a1f0d964ef67be4d5ecf2073
InvalidPointer <invalpoint@gmail.com>
2016-05-18 05:18:53 +0300

    Added exception handling in main

--- main.cpp
+++ main.cpp
@@ -7,31 +7,36 @@ using namespace std;
 int main(int argc, char **argv)
 {
     if (argc < 2 || argc > 3) {
-        cout << "Wrong number of params! Usage: " << argv[0] << " %regexp% [search]" << endl;
+        cerr << "Wrong number of params! Usage: " << argv[0] << " %regexp% [search]" << endl;
         return 0;
     }
 
-    Regexp r(argv[1]);
-    if (argc == 3) {
-        if (!strcmp(argv[2], "search")) {
-            string str;
-            while (cin >> str) {
-                rc_result res = r.search(str);
-                if (res.status) {
-                    cout << res.result << endl;
+    try {
+        Regexp r(argv[1]);
+
+        if (argc == 3) {
+            if (!strcmp(argv[2], "search")) {
+                string str;
+                while (cin >> str) {
+                    rc_result res = r.search(str);
+                    if (res.status) {
+                        cout << res.result << endl;
+                    }
                 }
+            } else {
+                cerr << "Wrong param! Usage: " << argv[0] << " %regexp% [search]" << endl;
+                return 0;
             }
         } else {
-            cout << "Wrong param! Usage: " << argv[0] << " %regexp% [search]" << endl;
-            return 0;
-        }
-    } else {
-        string str;
-        while (cin >> str) {
-            if (r.match(str)) {
-                cout << str << endl;
+            string str;
+            while (cin >> str) {
+                if (r.match(str)) {
+                    cout << str << endl;
+                }
             }
         }
+    } catch (const std::exception & ex) {
+        cerr << ex.what() << endl;
     }
 
     return 0;
