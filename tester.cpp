559a3ed5c1c70dc9cb8f991464f83e0ac4ad4f8f
InvalidPointer <invalpoint@gmail.com>
2016-05-26 01:16:29 +0300

    Added syntax check feature

--- tester.cpp
+++ tester.cpp
@@ -13,15 +13,25 @@ struct test
     vector<int> lines;
 };
 
+enum mode { MATCH, SEARCH, SYNTAX };
+
 vector<string> get_files_in_dir(const string &dir_name);
 
 int main(int argc, char **argv)
 {
     if (argc != 3) {
-        cout << "Wrong params! Usage: " << argv[0] << " %path_to_tests% {match|search}" << endl;
+        cout << "Wrong params! Usage: " << argv[0] << " %path_to_tests% {match|search|syntax}" << endl;
         return 0;
     }
-    int mode = !string(argv[2]).compare("search");
+
+    mode mode;
+    if (!string(argv[2]).compare("match")) {
+        mode = MATCH;
+    } else if (!string(argv[2]).compare("search")) {
+        mode = SEARCH;
+    } else {
+        mode = SYNTAX;
+    }
 
     string tests_dir = argv[1];
     vector<string> test_fnames = get_files_in_dir(tests_dir + "/expressions");
@@ -30,23 +40,52 @@ int main(int argc, char **argv)
         test t{-1};
 
         ifstream expr_file (tests_dir + "/expressions/" + *it);
+        ifstream res_file (tests_dir + "/answers/" + *it);
         string expr;
+
+        if (mode == SYNTAX) {
+            int line = 0;
+            while (expr_file >> expr) {
+                string res;
+                res_file >> res;
+
+                try {
+                    Regexp r(expr);
+                    if (res.compare("Correct")) {
+                        t.num = it - test_fnames.begin();
+                        t.lines.push_back(line);
+                    }
+                } catch (...) {
+                    if (!res.compare("Correct")) {
+                        t.num = it - test_fnames.begin();
+                        t.lines.push_back(line);
+                    }
+                }
+                line++;
+            }
+
+            if (t.num != -1) {
+                error_tests.push_back(t);
+            }
+
+            continue;
+        }
+
         expr_file >> expr;
         Regexp r(expr);
 
         ifstream str_file (tests_dir + "/strings/" + *it);
-        ifstream res_file (tests_dir + "/answers/" + *it);
         string test_string;
         int line = 0;
         while (str_file >> test_string) {
-            if (!mode) {
+            if (mode == MATCH) {
                 bool res;
                 res_file >> res;
                 if (r.match(test_string) != res) {
                     t.num = it - test_fnames.begin();
                     t.lines.push_back(line);
                 }
-            } else {
+            } else if (mode == SEARCH) {
                 string res;
                 res_file >> res;
                 rc_result rc_res = r.search(test_string);
